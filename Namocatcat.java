e42f8cedbd090e80c2ee35079f4b6cde58648b12
Namocatcat <romeljaspernamocatcat@gmail.com>
2024-02-13 19:13:54 +0800

    Create Namocatcat.java

---
+++ Namocatcat.java
@@ -0,0 +1,27 @@
+package namocatcat;
+
+import java.util.Scanner;
+
+public class Namocatcat {
+    
+    static Scanner InputNum = new Scanner (System.in);
+    static Scanner InputString= new Scanner (System.in);
+    
+    public static void main(String[] args) {
+    System.out.print("Name: ");
+    String name = InputString.nextLine();
+    System.out.print("Age: ");
+    int age = InputNum.nextInt();
+    System.out.print("Gender: ");
+    String gender = InputString.nextLine();
+    System.out.print("Address: ");
+    String address = InputString.nextLine();
+    System.out.print("Civil Status: ");
+    String Civil_Status = InputString.nextLine();
+    System.out.print("# of Brothers/Sisters: ");
+    int Brothers_And_Sisters = InputNum.nextInt();
+    
+    
+    }
+    
+}
